a5b6243f017c95003968f6bc23fcf7abcfff6138
Gavin Hensley <ghensl11@wgu.edu>
2025-04-29 06:02:30 -0700

    Create repository interfaces - Complete part E

---
+++ travelagency/src/main/java/com/example/travelagency/dao/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.travelagency.dao;
+
+import com.example.travelagency.entities.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
