052af0be1b6f15ce1052a9f2c666e0816a52757f
Gavin Hensley <ghensl11@wgu.edu>
2025-04-30 13:12:44 -0700

    Complete part H - Create checkout controller

---
+++ travelagency/src/main/java/com/example/travelagency/controllers/CheckoutController.java
@@ -0,0 +1,25 @@
+package com.example.travelagency.controllers;
+
+import com.example.travelagency.services.CheckoutService;
+import com.example.travelagency.services.Purchase;
+import com.example.travelagency.services.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/checkout")
+@CrossOrigin(origins = "*")
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/placeOrder")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        return checkoutService.placeOrder(purchase);
+    }
+}
